1378be6992130592716899a76b7f71103e982b46
spont4e <spont4e@gmail.com>
2017-08-18 15:11:05 +0300

    Add all files to git

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.textRealName = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(478, 76);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(140, 65);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.TabStop = false;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(478, 163);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(140, 56);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.TabStop = false;
+            this.buttonStop.Text = "Стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textUsername
+            // 
+            this.textUsername.Location = new System.Drawing.Point(127, 76);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.ReadOnly = true;
+            this.textUsername.Size = new System.Drawing.Size(302, 20);
+            this.textUsername.TabIndex = 2;
+            this.textUsername.TabStop = false;
+            // 
+            // textRealName
+            // 
+            this.textRealName.Location = new System.Drawing.Point(127, 121);
+            this.textRealName.Name = "textRealName";
+            this.textRealName.ReadOnly = true;
+            this.textRealName.Size = new System.Drawing.Size(302, 20);
+            this.textRealName.TabIndex = 3;
+            this.textRealName.TabStop = false;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(130, 163);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.ReadOnly = true;
+            this.textEmail.Size = new System.Drawing.Size(299, 20);
+            this.textEmail.TabIndex = 4;
+            this.textEmail.TabStop = false;
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(131, 199);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.ReadOnly = true;
+            this.textPhone.Size = new System.Drawing.Size(298, 20);
+            this.textPhone.TabIndex = 5;
+            this.textPhone.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Потребител";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Име";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "еПоща";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Телефон";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHeader.Location = new System.Drawing.Point(39, 19);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(561, 25);
+            this.labelHeader.TabIndex = 10;
+            this.labelHeader.Text = "В томболата участват 00000 потребители на Networx-BG !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 253);
+            this.Controls.Add(this.labelHeader);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textRealName);
+            this.Controls.Add(this.textUsername);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Томбола";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.TextBox textRealName;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelHeader;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public string[][] data;
+        public int randomNumber;
+   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath = Application.StartupPath+"\\tombola.csv";
+            StreamReader sr = new StreamReader(filePath);
+            var lines = new List<string[]>();
+            int Row = 0;
+            while (!sr.EndOfStream)
+            {
+                string[] Line = sr.ReadLine().Split(',');
+                lines.Add(Line);
+                Row++;
+            }
+            data = lines.ToArray();
+            labelHeader.Text = "В томболата участват "+data.Length+" потребители на Networx-BG !";
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (data.Length == 0)
+            {
+                timer1.Enabled = false;
+                buttonStop.Enabled = false;
+                MessageBox.Show("Няма повече потребители за изтегляне!");
+                return;
+            }
+            Random random = new Random();
+            randomNumber  = random.Next(0, data.Length);
+            textUsername.Text = data[randomNumber][0];
+            textRealName.Text = data[randomNumber][1];
+            int found = 0;
+            found = data[randomNumber][2].LastIndexOf("@");
+            int zvezdichki = found;
+            if (found > 15) zvezdichki = 15;
+            if(found>0) {
+                textEmail.Text = "***************".Substring(0, zvezdichki) + "@" + data[randomNumber][2].Substring(found + 1);
+            }
+            else
+            {
+                textEmail.Text = "";
+            }
+            if (data[randomNumber][3].Length > 4)
+            {
+                textPhone.Text = data[randomNumber][3].Substring(0, 3)+"*******";
+            }
+            else { 
+                textPhone.Text = data[randomNumber][3];
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+
+            string[][] dest = new string[data.Length - 1][];
+            if (randomNumber > 0)
+                Array.Copy(data, 0, dest, 0, randomNumber);
+
+            if (randomNumber < data.Length - 1)
+                Array.Copy(data, randomNumber + 1, dest, randomNumber, data.Length - randomNumber - 1);
+
+            data = dest;
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (data.Length == 0) return;
+            if (e.KeyCode == Keys.Enter) buttonStart_Click(null, EventArgs.Empty);
+            if (e.KeyCode == Keys.Space) buttonStop_Click(null, EventArgs.Empty);
+        }
+
+    }
+}
